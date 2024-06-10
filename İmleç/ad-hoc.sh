bc25ca2882347b08a99c83b02289e8637d84ae19
Muhammet Halak <75750279+halak0013@users.noreply.github.com>
2024-06-10 16:03:45 +0300

    Update ad-hoc.sh

--- İmleç/ad-hoc.sh
+++ İmleç/ad-hoc.sh
@@ -1,4 +1,6 @@
+echo "ag ayarlari kapaniyor"
 nmcli dev set wlan0 managed no
+sleep 5
 
 IP_ADDR=192.160.0.1
 IFACE=wlan0
@@ -10,13 +12,27 @@ SUBNET="24"
 BRIDGE_IFACE="mesh-bridge"
 BATMAN_IFACE="bat0"
 
+echo "ibss ayarlaniyor"
 iw dev $IFACE set type ibss
+sleep 5
+
+echo "mtu ayaralniyor"
 ip link set up mtu 1500 dev $IFACE
+sleep 5
+
+echo "ibss agi olusturuluyor"
 iw dev $IFACE ibss join $MESH_ID $FREQUENCY  fixed-freq $MESH_MAC
+sleep 5
 
-
+echo "batctl'ye ekleniyor"
 batctl if add $IFACE
+sleep 5
+
+echo "bat0 ayarlaniyor"
 ip link set up dev $BATMAN_IFACE
+sleep 5
+
+echo "ip atamasi yapılıyor"
 ip addr add "${IP_ADDR}/${SUBNET}" dev $BATMAN_IFACE
 
 
